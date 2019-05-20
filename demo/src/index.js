7b45bf41fa5b56952f0ae213ee7bb6452acecd01
Lauren Ashpole <lauren@laurenashpole.com>
2019-05-20 17:32:49 -0400

    Demo nav and bg styling

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
 import Index from './pages/Index';
 import Demos from './pages/Demos';
 import Docs from './pages/Docs';
@@ -12,15 +12,27 @@ const Demo = () => {
     <Fragment>
       <nav className="nav">
         <ul className="nav__list nav__list--secondary">
-          <li className="nav__item"><a href="https://github.com/laurenashpole/react-inner-image-zoom">Github</a></li>
-          <li className="nav__item"><a href="#">NPM</a></li>
+          <li className="nav__item">
+            <a className="nav__link" href="https://github.com/laurenashpole/react-inner-image-zoom">Github</a>
+          </li>
+          <li className="nav__item">
+            <a className="nav__link" href="#">NPM</a>
+          </li>
         </ul>
 
         <ul className="nav__list nav__list--primary">
-          <li className="nav__item nav__item--left"><Link to="/">Inner Image Zoom</Link></li>
-          <li className="nav__item"><Link to="/demos">Demos</Link></li>
-          <li className="nav__item"><Link to="/docs">Docs</Link></li>
-          <li className="nav__item"><Link to="/support">Support</Link></li>
+          <li className="nav__item nav__item--left">
+            <NavLink className="nav__link" to="/">Inner Image Zoom</NavLink>
+          </li>
+          <li className="nav__item">
+            <NavLink to="/demos" className="nav__link" activeClassName="nav__link--active">Demos</NavLink>
+          </li>
+          <li className="nav__item">
+            <NavLink to="/docs" className="nav__link" activeClassName="nav__link--active">Docs</NavLink>
+          </li>
+          <li className="nav__item">
+            <NavLink to="/support" className="nav__link" activeClassName="nav__link--active">Support</NavLink>
+          </li>
         </ul>
       </nav>
 
