ad43307b1f005adb6c936a8c015b4bfdea57f51f
Lauren Ashpole <lauren@laurenashpole.com>
2019-05-21 00:20:41 -0400

    Started carousel demo

--- demo/src/pages/Demos.js
+++ demo/src/pages/Demos.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import Slick from 'react-slick';
 import ThumbnailGallery from '../components/ThumbnailGallery';
+import InnerImageZoom from '../../../src';
+import '../stylesheets/slick.css';
+import '../stylesheets/slick-theme.css';
 
 const Demos = () => {
   return(
@@ -20,6 +24,14 @@ const Demos = () => {
           src: '/unsplash-4.jpg',
           zoomSrc: '/unsplash-4-large.jpg'
         }]} />
+      </div>
+
+      <div>
+        <Slick>
+          <InnerImageZoom src="/unsplash-1.jpg" zoomSrc="/unsplash-1-large.jpg" fullscreenOnMobile={false} />
+          <InnerImageZoom src="/unsplash-2.jpg" zoomSrc="/unsplash-2-large.jpg" fullscreenOnMobile={false} />
+          <InnerImageZoom src="/unsplash-3.jpg" zoomSrc="/unsplash-3-large.jpg" fullscreenOnMobile={false} />
+        </Slick>
       </div>
     </div>
   );
