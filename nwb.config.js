2492e063086a4a33280f4c564c7f4ff9c6d88033
Ralph Lawrence <ralph@zerojargon.com>
2020-10-12 10:50:55 +0100

    BD-10887 - Adjust babel configuration for older iOS browsers

--- nwb.config.js
+++ nwb.config.js
@@ -3,5 +3,10 @@ module.exports = {
   npm: {
     esModules: true,
     umd: false
+  },
+  babel: {
+    env: {
+      targets: ">1% or last 2 versions, not dead, not ie <= 11, not op_mini all, not ie_mob <= 11"
+    }
   }
 }
