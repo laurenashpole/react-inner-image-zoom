75b2a66884ecfbddbaea0d077218ed27feaab38f
Max d'Ayala <max@dayala.co.uk>
2022-01-13 10:54:24 +0000

    fix(InnerImageZoom): Fix a bug in Firefox when the cursor is dragged to the left side of the window.
    This bug affects Firefox when zoomed images are dragged.
    
    When the cursor is dragged to the left side of the browser window the e.pageX value can be 0.
    This zero x offset is interpreted as a falsy value by the code and e.changedTouches[0].pageX
    is executed instead. This causes an exception because e.changedTouches is not defined.
    
    This change allows an x offset of 0 to be recognised as a valid value in function handleDragMove.
    
    The same fix is applied to function handleDragStart for consistency.

--- src/InnerImageZoom/InnerImageZoom.js
+++ src/InnerImageZoom/InnerImageZoom.js
@@ -100,12 +100,10 @@ const InnerImageZoom = ({
   };
 
   const handleDragStart = (e) => {
-    imgProps.current.offsets = getOffsets(
-      e.pageX || e.changedTouches[0].pageX,
-      e.pageY || e.changedTouches[0].pageY,
-      zoomImg.current.offsetLeft,
-      zoomImg.current.offsetTop
-    );
+    const pageX = typeof e.pageX === 'number' ? e.pageX : e.changedTouches[0].pageX;
+    const pageY = typeof e.pageY === 'number' ? e.pageY : e.changedTouches[0].pageY;
+    //console.log(`Start pageX: ${pageX} pageY: ${pageY}`);
+    imgProps.current.offsets = getOffsets(pageX, pageY, zoomImg.current.offsetLeft, zoomImg.current.offsetTop);
 
     setIsDragging(true);
 
@@ -119,8 +117,11 @@ const InnerImageZoom = ({
 
   const handleDragMove = useCallback((e) => {
     e.stopPropagation();
-    let left = (e.pageX || e.changedTouches[0].pageX) - imgProps.current.offsets.x;
-    let top = (e.pageY || e.changedTouches[0].pageY) - imgProps.current.offsets.y;
+    const pageX = typeof e.pageX === 'number' ? e.pageX : e.changedTouches[0].pageX;
+    const pageY = typeof e.pageY === 'number' ? e.pageY : e.changedTouches[0].pageY;
+    //console.log(`Move pageX: ${pageX} pageY: ${pageY}`);
+    let left = pageX - imgProps.current.offsets.x;
+    let top = pageY - imgProps.current.offsets.y;
 
     left = Math.max(Math.min(left, 0), (imgProps.current.scaledDimensions.width - imgProps.current.bounds.width) * -1);
     top = Math.max(Math.min(top, 0), (imgProps.current.scaledDimensions.height - imgProps.current.bounds.height) * -1);
