7675e842cbfdb10f5ef42e39d2eb92fd166a2f4a
laurenashpole <lauren@laurenashpole.com>
2022-06-12 17:46:20 -0400

    Upgraded to react-inner-image-zoom version 3.0.1

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5c456e9e3fdbbec84967";
+/******/ 	var hotCurrentHash = "1dd4b565088aaec658e1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
